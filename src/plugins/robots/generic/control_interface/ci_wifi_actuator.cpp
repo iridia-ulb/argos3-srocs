f0e6af15c5c7ac3537af5a00b8d2c086eb5f0cc7
Michael Allwright <allsey87@gmail.com>
2019-12-18 15:34:37 +0100

    Added support for sending and receiving CLuaVector2, CLuaVector3, and CLuaQuaternion over WiFi

--- src/plugins/robots/generic/control_interface/ci_wifi_actuator.cpp
+++ src/plugins/robots/generic/control_interface/ci_wifi_actuator.cpp
@@ -7,7 +7,14 @@
 #include "ci_wifi_actuator.h"
 
 #ifdef ARGOS_WITH_LUA
-#include <argos3/core/wrappers/lua/lua_utility.h>
+   #include <argos3/core/wrappers/lua/lua_utility.h>
+   #include <argos3/core/wrappers/lua/lua_vector2.h>
+   #include <argos3/core/wrappers/lua/lua_vector3.h>
+   #include <argos3/core/wrappers/lua/lua_quaternion.h>
+   /* attention: these definitions must match those in ci_wifi_sensor.cpp */
+   #define LUA_TUSERDATA_VECTOR2 1
+   #define LUA_TUSERDATA_VECTOR3 2
+   #define LUA_TUSERDATA_QUATERNION 3
 #endif
 
 namespace argos {
@@ -42,6 +49,50 @@ namespace argos {
          case LUA_TTABLE:
             LuaSerializeTable(cKeyValueBytes, pt_lua_state, -2);
             break;
+         case LUA_TUSERDATA:
+            if (lua_getmetatable(pt_lua_state, -2)) {
+               /* push the vector2 metatable onto the stack */
+               lua_getfield(pt_lua_state, LUA_REGISTRYINDEX, CLuaVector2::GetTypeId().c_str());  /* is it a vector2? */
+               if (lua_rawequal(pt_lua_state, -2, -1)) {
+                  /* pop both the two metatables off the stack */
+                  lua_pop(pt_lua_state, 2);
+                  cKeyValueBytes << static_cast<UInt8>(LUA_TUSERDATA_VECTOR2);
+                  const CVector2& cVector2 = *static_cast<CVector2*>(lua_touserdata(pt_lua_state, -2));
+                  cKeyValueBytes << cVector2.GetX() << cVector2.GetY();
+               }
+               else {
+                  /* pop the vector2 metatable off the stack */
+                  lua_pop(pt_lua_state, 1);
+                  /* push the vector3 metatable onto the stack */
+                  lua_getfield(pt_lua_state, LUA_REGISTRYINDEX, CLuaVector3::GetTypeId().c_str());  /* is it a vector3? */
+                  if (lua_rawequal(pt_lua_state, -2, -1)) {
+                     /* pop both the two metatables off the stack */
+                     lua_pop(pt_lua_state, 2);
+                     cKeyValueBytes << static_cast<UInt8>(LUA_TUSERDATA_VECTOR3);
+                     const CVector3& cVector3 = *static_cast<CVector3*>(lua_touserdata(pt_lua_state, -2));
+                     cKeyValueBytes << cVector3.GetX() << cVector3.GetY() << cVector3.GetZ();
+                  }
+                  else {
+                     /* pop the vector3 metatable off the stack */
+                     lua_pop(pt_lua_state, 1);
+                     /* push the quaternion metatable onto the stack */
+                     lua_getfield(pt_lua_state, LUA_REGISTRYINDEX, CLuaQuaternion::GetTypeId().c_str());  /* is it a quaternion? */
+                     if (lua_rawequal(pt_lua_state, -2, -1)) {
+                        /* pop both the metatables off the stack */
+                        lua_pop(pt_lua_state, 2);
+                        cKeyValueBytes << static_cast<UInt8>(LUA_TUSERDATA_QUATERNION);
+                        const CQuaternion& cQuaternion = *static_cast<CQuaternion*>(lua_touserdata(pt_lua_state, -2));
+                        cKeyValueBytes << cQuaternion.GetW() << cQuaternion.GetX() << cQuaternion.GetY() << cQuaternion.GetZ();
+                     }
+                     else {
+                        /* unrecognized userdata subtype detected */
+                        lua_pushstring(pt_lua_state, "Unrecognized userdata subtype detected in table");
+                        lua_error(pt_lua_state);
+                     }
+                  }
+               }
+            }
+            break;
          default:
             /* key type not recognized, move to the next pair */
             lua_pop(pt_lua_state, 1);
@@ -62,6 +113,50 @@ namespace argos {
             break;
          case LUA_TTABLE:
             LuaSerializeTable(cKeyValueBytes, pt_lua_state, -1);
+            break;
+         case LUA_TUSERDATA:
+            if (lua_getmetatable(pt_lua_state, -1)) {
+               /* push the vector2 metatable onto the stack */
+               lua_getfield(pt_lua_state, LUA_REGISTRYINDEX, CLuaVector2::GetTypeId().c_str());  /* is it a vector2? */
+               if (lua_rawequal(pt_lua_state, -2, -1)) {
+                  /* pop both the two metatables off the stack */
+                  lua_pop(pt_lua_state, 2);
+                  cKeyValueBytes << static_cast<UInt8>(LUA_TUSERDATA_VECTOR2);
+                  const CVector2& cVector2 = *static_cast<CVector2*>(lua_touserdata(pt_lua_state, -1));
+                  cKeyValueBytes << cVector2.GetX() << cVector2.GetY();
+               }
+               else {
+                  /* pop the vector2 metatable off the stack */
+                  lua_pop(pt_lua_state, 1);
+                  /* push the vector3 metatable onto the stack */
+                  lua_getfield(pt_lua_state, LUA_REGISTRYINDEX, CLuaVector3::GetTypeId().c_str());  /* is it a vector3? */
+                  if (lua_rawequal(pt_lua_state, -2, -1)) {
+                     /* pop both the two metatables off the stack */
+                     lua_pop(pt_lua_state, 2);
+                     cKeyValueBytes << static_cast<UInt8>(LUA_TUSERDATA_VECTOR3);
+                     const CVector3& cVector3 = *static_cast<CVector3*>(lua_touserdata(pt_lua_state, -1));
+                     cKeyValueBytes << cVector3.GetX() << cVector3.GetY() << cVector3.GetZ();
+                  }
+                  else {
+                     /* pop the vector3 metatable off the stack */
+                     lua_pop(pt_lua_state, 1);
+                     /* push the quaternion metatable onto the stack */
+                     lua_getfield(pt_lua_state, LUA_REGISTRYINDEX, CLuaQuaternion::GetTypeId().c_str());  /* is it a quaternion? */
+                     if (lua_rawequal(pt_lua_state, -2, -1)) {
+                        /* pop both the metatables off the stack */
+                        lua_pop(pt_lua_state, 2);
+                        cKeyValueBytes << static_cast<UInt8>(LUA_TUSERDATA_QUATERNION);
+                        const CQuaternion& cQuaternion = *static_cast<CQuaternion*>(lua_touserdata(pt_lua_state, -1));
+                        cKeyValueBytes << cQuaternion.GetW() << cQuaternion.GetX() << cQuaternion.GetY() << cQuaternion.GetZ();
+                     }
+                     else {
+                        /* unrecognized userdata subtype detected */
+                        lua_pushstring(pt_lua_state, "Unrecognized userdata subtype detected in table");
+                        lua_error(pt_lua_state);
+                     }
+                  }
+               }
+            }
             break;
          default:
             /* value type not recognized, move to the next pair */
--- src/plugins/robots/generic/control_interface/ci_wifi_sensor.cpp
+++ src/plugins/robots/generic/control_interface/ci_wifi_sensor.cpp
@@ -7,7 +7,14 @@
 #include "ci_wifi_sensor.h"
 
 #ifdef ARGOS_WITH_LUA
-#include <argos3/core/wrappers/lua/lua_utility.h>
+   #include <argos3/core/wrappers/lua/lua_utility.h>
+   #include <argos3/core/wrappers/lua/lua_vector2.h>
+   #include <argos3/core/wrappers/lua/lua_vector3.h>
+   #include <argos3/core/wrappers/lua/lua_quaternion.h>
+   /* attention: these definitions must match those in ci_wifi_actuator.cpp */
+   #define LUA_TUSERDATA_VECTOR2 1
+   #define LUA_TUSERDATA_VECTOR3 2
+   #define LUA_TUSERDATA_QUATERNION 3
 #endif
 
 namespace argos {
@@ -31,8 +38,9 @@ namespace argos {
    void LuaDeserializeTable(CByteArray& c_data, lua_State* pt_lua_state) {
       SInt8 nKeyType, nValueType;
       std::string strBuffer;
-      Real fBuffer;
+      Real fBuffer[4];
       SInt8 nBuffer;
+      UInt8 unUserDataSubtype;
       bool bContinue = true;
       while(bContinue && !c_data.Empty()) {
          c_data >> nKeyType;
@@ -42,8 +50,8 @@ namespace argos {
             lua_pushstring(pt_lua_state, strBuffer.c_str());
             break;
          case LUA_TNUMBER:
-            c_data >> fBuffer;
-            lua_pushnumber(pt_lua_state, fBuffer);
+            c_data >> fBuffer[0];
+            lua_pushnumber(pt_lua_state, fBuffer[0]);
             break;
          case LUA_TBOOLEAN:
             c_data >> nBuffer;
@@ -52,6 +60,28 @@ namespace argos {
          case LUA_TTABLE:
             lua_newtable (pt_lua_state);
             LuaDeserializeTable(c_data, pt_lua_state);
+            break;
+         case LUA_TUSERDATA:
+            c_data >> unUserDataSubtype;
+            switch(unUserDataSubtype) {
+            case LUA_TUSERDATA_VECTOR2:
+               c_data >> fBuffer[0] >> fBuffer[1];
+               CLuaVector2::PushVector2(pt_lua_state, fBuffer[0], fBuffer[1]);
+               break;
+            case LUA_TUSERDATA_VECTOR3:
+               c_data >> fBuffer[0] >> fBuffer[1] >> fBuffer[2];
+               CLuaVector3::PushVector3(pt_lua_state, fBuffer[0], fBuffer[1], fBuffer[2]);
+               break;
+            case LUA_TUSERDATA_QUATERNION:
+               c_data >> fBuffer[0] >> fBuffer[1] >> fBuffer[2] >> fBuffer[3];
+               CLuaQuaternion::PushQuaternion(pt_lua_state, fBuffer[0], fBuffer[1], fBuffer[2], fBuffer[3]);
+               break;
+            default:
+               /* key is not a recognized subtype of userdata */
+               bContinue = false;
+               continue;
+               break;
+            }
             break;
          default:
             /* sentinel or the key is not a recognized type */
@@ -66,8 +96,8 @@ namespace argos {
             lua_pushstring(pt_lua_state, strBuffer.c_str());
             break;
          case LUA_TNUMBER:
-            c_data >> fBuffer;
-            lua_pushnumber(pt_lua_state, fBuffer);
+            c_data >> fBuffer[0];
+            lua_pushnumber(pt_lua_state, fBuffer[0]);
             break;
          case LUA_TBOOLEAN:
             c_data >> nBuffer;
@@ -76,6 +106,30 @@ namespace argos {
          case LUA_TTABLE:
             lua_newtable (pt_lua_state);
             LuaDeserializeTable(c_data, pt_lua_state);
+            break;
+         case LUA_TUSERDATA:
+            c_data >> unUserDataSubtype;
+            switch(unUserDataSubtype) {
+            case LUA_TUSERDATA_VECTOR2:
+               c_data >> fBuffer[0] >> fBuffer[1];
+               CLuaVector2::PushVector2(pt_lua_state, fBuffer[0], fBuffer[1]);
+               break;
+            case LUA_TUSERDATA_VECTOR3:
+               c_data >> fBuffer[0] >> fBuffer[1] >> fBuffer[2];
+               CLuaVector3::PushVector3(pt_lua_state, fBuffer[0], fBuffer[1], fBuffer[2]);
+               break;
+            case LUA_TUSERDATA_QUATERNION:
+               c_data >> fBuffer[0] >> fBuffer[1] >> fBuffer[2] >> fBuffer[3];
+               CLuaQuaternion::PushQuaternion(pt_lua_state, fBuffer[0], fBuffer[1], fBuffer[2], fBuffer[3]);
+               break;
+            default:
+               /* key is not a recognized subtype of userdata */
+               bContinue = false;
+               /* remove the key from the stack */
+               lua_pop(pt_lua_state, 1);
+               continue;
+               break;
+            }
             break;
          default:
             /* the value is not a recognized type */
