18c085bd875a47724544f85802abb106e4c2c36f
Weixu Zhu <freedomcondor@126.com>
2020-07-23 15:34:48 +0200

    remove unnecessary code for sample rules

--- src/testing/builderbot/simulator/sample_rules.lua
+++ src/testing/builderbot/simulator/sample_rules.lua
@@ -16,22 +16,6 @@ rules.list = {
       },
       generate_orientations = false
    },
-   --[[
-   {
-      rule_type = 'place',
-      structure = {
-         {
-            index = vector3(0, 0, 0),
-            type = 1
-         },
-      },
-      target = {
-         reference_index = vector3(0, 0, 0),
-         offset_from_reference = vector3(1, 0, 0),
-      },
-      generate_orientations = false
-   },
-   ]]
    {
       rule_type = 'place',
       structure = {
