613e42acfd978322550f7f90680282a396ba2547
Weixu Zhu <freedomcondor@126.com>
2020-10-01 14:48:53 +0200

    Switch rangefinder ids (In hardware, No 6 and 7 are the ones in front), adjust rangefinder maximum distance parameter from 0.05 to 0.045 (In hardware, the readings could be 0.0499 when nothing is in front of the rangefinder)

--- src/plugins/robots/builderbot/control_interface/lua/api/parameters.lua
+++ src/plugins/robots/builderbot/control_interface/lua/api/parameters.lua
@@ -9,7 +9,7 @@ return {
     block_position_tolerance = tonumber(robot.params.block_position_tolerance or 0.001),
     proximity_touch_tolerance = tonumber(robot.params.proximity_touch_tolerance or 0.003),
     proximity_detect_tolerance = tonumber(robot.params.proximity_detect_tolerance or 0.03),
-    proximity_maximum_distance = tonumber(robot.params.proximity_maximum_distance or 0.05),
+    proximity_maximum_distance = tonumber(robot.params.proximity_maximum_distance or 0.045),
     lift_system_rf_cover_threshold = tonumber(robot.params.lift_system_rf_cover_threshold or 0.06),
     lift_system_position_tolerance = tonumber(robot.params.lift_system_position_tolerance or 0.001),
     obstacle_avoidance_backup = tonumber(robot.params.obstacle_avoidance_backup or 0.08),
