7fa8e163e3140559b761941e23090ec0d17deb13
Michael Allwright <allsey87@gmail.com>
2020-10-08 13:19:41 +0200

    Add rangefinders test

---
+++ src/testing/pi-puck/hardware/test_rangefinders.lua
@@ -0,0 +1,30 @@
+--[[ This function is executed every time you press the 'execute' button ]]
+function init()
+end
+
+
+--[[ This function is executed at each time step
+     It must contain the logic of your controller ]]
+function step()
+   for index, device in ipairs(robot.rangefinders) do
+      local message = 
+         string.format('%d: distance = %f, illumination = %f', index,
+                       device.reading.proximity, device.reading.illumination)
+      log(message)
+   end
+
+end
+
+--[[ This function is executed every time you press the 'reset'
+     button in the GUI. It is supposed to restore the state
+     of the controller to whatever it was right after init() was
+     called. The state of sensors and actuators is reset
+     automatically by ARGoS. ]]
+function reset()
+end
+
+--[[ This function is executed only once, when the robot is removed
+     from the simulation ]]
+function destroy()
+   -- put your code here
+end
