c5d2aa28197dfaec70f63a058dc336b06b4beb33
Michael Allwright <allsey87@gmail.com>
2020-10-08 11:50:12 +0200

    Clean up the differential drive sensor and add testing

--- src/plugins/robots/pi-puck/hardware/pipuck_differential_drive_default_sensor.cpp
+++ src/plugins/robots/pi-puck/hardware/pipuck_differential_drive_default_sensor.cpp
@@ -57,26 +57,21 @@ namespace argos {
          /* Get context and trigger */
          iio_context* psContext = CPiPuck::GetInstance().GetContext();
          iio_device* psUpdateTrigger = CPiPuck::GetInstance().GetSensorUpdateTrigger();
-         /* Parse the device name and channel names */
-         std::string strDevice, strLeft, strRight;
-         GetNodeAttribute(t_tree, "device", strDevice);
-         GetNodeAttribute(t_tree, "left", strLeft);
-         GetNodeAttribute(t_tree, "right", strRight);
          /* get a pointer to the device */
-         m_psDevice = ::iio_context_find_device(psContext, strDevice.c_str());
+         m_psDevice = ::iio_context_find_device(psContext, "epuck-encoders");
          if(m_psDevice == nullptr) {
-            THROW_ARGOSEXCEPTION("Could not find IIO device \"" << strDevice << "\"");
+            THROW_ARGOSEXCEPTION("Could not find IIO device \"epuck-encoders\"");
          }
          /* get the channels */
-         m_psLeft = ::iio_device_find_channel(m_psDevice, strLeft.c_str(), false);
+         m_psLeft = ::iio_device_find_channel(m_psDevice, "angl0", false);
          if(m_psLeft == nullptr) {
-            THROW_ARGOSEXCEPTION("Could not find IIO channel \"" << strLeft <<
-                                 "\" for device \"" << strDevice << "\"");
+            THROW_ARGOSEXCEPTION("Could not find IIO channel \"angl0\" for device "
+                                 "\"epuck-encoders\"");
          }
-         m_psRight = ::iio_device_find_channel(m_psDevice, strRight.c_str(), false);
+         m_psRight = ::iio_device_find_channel(m_psDevice, "angl1", false);
          if(m_psRight == nullptr) {
-            THROW_ARGOSEXCEPTION("Could not find IIO channel \"" << strRight <<
-                                 "\" for device \"" << strDevice << "\"");
+            THROW_ARGOSEXCEPTION("Could not find IIO channel \"angl1\" for device "
+                                 "\"epuck-encoders\"");
          }
          /* enable channels */
          ::iio_channel_enable(m_psLeft);
@@ -90,7 +85,7 @@ namespace argos {
          }
       }
       catch(CARGoSException& ex) {
-         THROW_ARGOSEXCEPTION_NESTED("Initialization error in the PiPuck differential drive sensor.", ex);
+         THROW_ARGOSEXCEPTION_NESTED("Initialization error in the Pi-Puck differential drive sensor.", ex);
       }
    }
   
@@ -138,7 +133,7 @@ namespace argos {
                    "Michael Allwright [allsey87@gmail.com]",
                    "1.0",
                    "The pipuck differential drive sensor.",
-                   "This sensor monitors the differential drive of the pipuck.",
+                   "This sensor monitors the differential drive of the Pi-Puck.",
                    "Usable"
    );
 
--- src/testing/pi-puck/hardware/test_differential_drive.lua
+++ src/testing/pi-puck/hardware/test_differential_drive.lua
@@ -1,13 +1,13 @@
 --[[ This function is executed every time you press the 'execute' button ]]
 function init()
-   config = 1
-   velocities = {
-      {1, 1}, -- forward
-      {-1, -1}, -- backwards
-      {1, -1}, -- cw
-      {-1, 1}, -- ccw
-      {0, 0} -- stop
+   settings = {
+      {'forward', 100, 100},
+      {'backwards', -100, -100},
+      {'clockwise', 100, -100},
+      {'counter-clockwise', -100, 100},
+      {'stop', 0, 0}
    }
+   count = 1
 end
 
 
@@ -15,10 +15,15 @@ end
      It must contain the logic of your controller ]]
 function step()
    -- set velocity
-   robot.differential_drive.set_target_velocity(unpack(velocities[config]))
+   local index = math.ceil(count / 10)
+   local desc, left, right = table.unpack(settings[index])
+   robot.differential_drive.set_target_velocity(left, right)
+   print(string.format('--- step = %d, setting = %s ---', count, desc))
+   print(string.format('left:  target_vel = %d, delta_pos = %d', left, robot.differential_drive.encoders.left))
+   print(string.format('right: target_vel = %d, delta_pos = %d', right, robot.differential_drive.encoders.right))
    -- increment the config variable
-   if config < 5 then
-      config = config + 1
+   if count < 25 then
+      count = count + 1
    end
 end
 
@@ -28,7 +33,7 @@ end
      called. The state of sensors and actuators is reset
      automatically by ARGoS. ]]
 function reset()
-   config = 1
+   count = 1
 end
 
 --[[ This function is executed only once, when the robot is removed
