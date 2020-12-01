d6d7470aacffa816ca79d4ba42669fc40b55b463
sinanOguz <soguz.ankara@gmail.com>
2020-12-01 09:12:29 +0100

    delay added before write command

--- src/plugins/robots/drone/hardware/drone_flight_system_default_actuator.cpp
+++ src/plugins/robots/drone/hardware/drone_flight_system_default_actuator.cpp
@@ -85,11 +85,9 @@ namespace argos {
          mavlink_message_t tMessage;
          mavlink_msg_set_position_target_local_ned_encode(unTargetSystem, 0, &tMessage, &tSetpoint);
          Write(tMessage);
+         cRate.Sleep();
 
          try {
-            cRate.Sleep();
-            mavlink_message_t tMessage;
-            mavlink_msg_set_position_target_local_ned_encode(unTargetSystem, 0, &tMessage, &tSetpoint);
             Write(tMessage);
          }
          catch(CARGoSException& ex) {
