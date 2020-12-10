1e2e6d9b43c4eb0184a77eb94a6e7f671faec9f5
sinanOguz <soguz.ankara@gmail.com>
2020-12-10 11:53:19 +0100

    Fix minor mistakes in the test configurations for the drone hardware

--- src/testing/drone/hardware/test_flight.lua
+++ src/testing/drone/hardware/test_flight.lua
@@ -1,53 +1,64 @@
---[[ This function is executed every time you press the 'execute' button ]]
 function init()
    state = 'preflight'
+   setpoint = 'none'
 end
 
---[[ This function is executed at each time step
-     It must contain the logic of your controller ]]
 function step()
-   print('time = ' .. tostring(robot.system.time))
-   print('pose = ' .. tostring(robot.flight_system.position) .. 
-         ' / ' .. tostring(robot.flight_system.orientation.z))
+   print('pose = ' .. tostring(robot.flight_system.position))
    print('state = ' .. state)
+   print('setpoint = ' .. setpoint)
    if robot.flight_system.ready() then
-      if state == 'preflight' then
-         print('enabling off-board mode')
-         robot.flight_system.set_offboard_mode(true)
-         -- WARNING this should make the drone take off
-         print('arming drone')
-         robot.flight_system.set_armed(true, false)
-         -- fly up one meter
-         robot.flight_system.set_target_pose(vector3(0,0,1), 0)
-         state = 'taking_off'
-      elseif state == 'taking_off' then
-         if math.abs(robot.flight_system.position.z + 1) < 0.025 then
-            robot.flight_system.set_target_pose(vector3(0,0,0), 0)
-            state = 'landing'
-         end
-      elseif state == 'landing' then
-         if math.abs(robot.flight_system.position.z) < 0.025 then
-            robot.flight_system.set_armed(false, false)
-            robot.flight_system.set_offboard_mode(false)
-            state = 'postflight'
-         end
-      end
-   end         
+       if state == 'preflight' then
+           print('arming drone')
+           robot.flight_system.set_armed(true, false)
+           robot.flight_system.set_offboard_mode(true)
+           robot.flight_system.set_target_pose(vector3(0,0,1), 0)
+           print('enabling off-board mode')
+           state = 'take_off'
+           setpoint = 'first'
+       elseif state == 'take_off' then
+           if setpoint == 'first' then
+               robot.flight_system.set_target_pose(vector3(0,0,1), 0)
+               if math.abs(robot.flight_system.position.z + 1) < 0.025 then
+                   robot.flight_system.set_target_pose(vector3(1,0,1), 0)
+                   setpoint = 'second'
+               end
+           elseif setpoint == 'second' then
+               robot.flight_system.set_target_pose(vector3(1,0,1), 0)
+               if math.abs(robot.flight_system.position.x - 1) < 0.025 then
+                   robot.flight_system.set_target_pose(vector3(1,1,1), 0)
+                   setpoint = 'third'
+               end
+           elseif setpoint == 'third' then
+               robot.flight_system.set_target_pose(vector3(1,1,1), 0)
+               if math.abs(robot.flight_system.position.y - 1) < 0.025 then
+                   robot.flight_system.set_target_pose(vector3(1,1,0), 0)
+                   setpoint = 'fourth'
+               end
+           elseif setpoint == 'fourth' then
+               robot.flight_system.set_target_pose(vector3(1,1,0), 0)
+               if math.abs(robot.flight_system.position.z + 0) < 0.025 then
+                   state = 'landing'
+                   setpoint = 'none'
+               end
+           end
+       elseif state == 'landing' then
+           robot.flight_system.set_offboard_mode(false)
+           robot.flight_system.set_armed(false, false)
+           state = 'postflight'
+       end
+   end
 end
 
---[[ This function is executed every time you press the 'reset'
-     button in the GUI. It is supposed to restore the state
-     of the controller to whatever it was right after init() was
-     called. The state of sensors and actuators is reset
-     automatically by ARGoS. ]]
+
 function reset()
 end
 
 --[[ This function is executed only once, when the robot is removed
-     from the simulation ]]
+    from the simulation ]]
 function destroy()
-   print('disarming drone')
-   robot.flight_system.set_armed(false, false)
-   print('disabling off-board mode')
-   robot.flight_system.set_offboard_mode(false)
+  print('disarming drone')
+  robot.flight_system.set_armed(false, false)
+  print('disabling off-board mode')
+  robot.flight_system.set_offboard_mode(false)
 end
