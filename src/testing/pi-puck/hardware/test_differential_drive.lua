b7711ab136a9309622e9fd707c13cdc3c195e839
Condor3397 <freedomcondor@126.com>
2020-12-15 09:43:33 +0100

    upgrade pipuck testing files for differential driving system

--- src/testing/pi-puck/hardware/test_differential_drive.lua
+++ src/testing/pi-puck/hardware/test_differential_drive.lua
@@ -1,14 +1,18 @@
 --[[ This function is executed every time you press the 'execute' button ]]
 function init()
-   local speed = 0.01  -- m
+   local forward_speed = 0.1 / 10  -- m      0.1m per 10s
+   local rotate_speed = math.pi * 0.0535 / 10  -- a full circle per 10s
+
    settings = {
-      {'forward', speed, speed},
-      {'backwards', -speed, -speed},
-      {'clockwise', speed, -speed},
-      {'counter-clockwise', -speed, speed},
+      {'clockwise', rotate_speed, rotate_speed},
+      {'counter-clockwise', -rotate_speed, -rotate_speed},
+      {'forward', forward_speed, -forward_speed},
+      {'backwards', -forward_speed, forward_speed},
       {'stop', 0, 0}
    }
+
    count = 1
+   index = 1
 end
 
 
@@ -16,14 +20,14 @@ end
      It must contain the logic of your controller ]]
 function step()
    -- set velocity
-   local index = math.ceil(count / 10)
+   local index = math.ceil(count / 50)  -- 50 ticks = 10s
    local desc, left, right = table.unpack(settings[index])
    robot.differential_drive.set_target_velocity(left, right)
    print(string.format('--- step = %d, setting = %s ---', count, desc))
    print(string.format('left:  target_vel = %f, delta_pos = %f', left, robot.differential_drive.encoders.left))
    print(string.format('right: target_vel = %f, delta_pos = %f', right, robot.differential_drive.encoders.right))
    -- increment the config variable
-   if count < 50 then
+   if count < 50 * 4 + 1 then
       count = count + 1
    end
 end
