e8eb1fca15aa132a5c70719efca050bbabae9aab
sinanOguz <soguz.ankara@gmail.com>
2021-01-24 13:01:13 +0100

    update

--- src/plugins/robots/drone/hardware/drone_flight_system_default_actuator.cpp
+++ src/plugins/robots/drone/hardware/drone_flight_system_default_actuator.cpp
@@ -88,7 +88,7 @@ namespace argos {
          // LOGERR << "Burdayin anuga goying: " << tSetpoint.z << std::endl;
          mavlink_message_t tMessage;
          mavlink_msg_set_position_target_local_ned_encode(unTargetSystem, 0, &tMessage, &tSetpoint);
-          // Write(tMessage);
+         Write(tMessage);
 
          try {
           //  cRate.Sleep();
