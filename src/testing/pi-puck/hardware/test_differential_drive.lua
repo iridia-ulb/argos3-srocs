567194ebfbbb1b71c10182bffcf9280b7001c2d8
Harry ZHU <freedomcondor@126.com>
2021-02-05 18:42:25 +0100

    Update test configurations and calibration for the Pi-Puck
    1. Change factor for pipuck hardware speed
    2. Update pipuck testing files for differential driving system
    3. Update comments for pipuck hardware differential drive system calibration
    4. Remove an unused line in pipuck hardware differential drive system testing
    5. Update comments for pipuck hardware test examples

--- src/testing/pi-puck/hardware/test_differential_drive.lua
+++ src/testing/pi-puck/hardware/test_differential_drive.lua
@@ -1,12 +1,16 @@
 --[[ This function is executed every time you press the 'execute' button ]]
 function init()
+   local forward_speed = 0.1 / 10  -- m      0.1m per 10s
+   local rotate_speed = math.pi * 0.0535 / 10  -- a full circle per 10s, 0.0535 is the distance between two wheels
+
    settings = {
-      {'forward', 100, 100},
-      {'backwards', -100, -100},
-      {'clockwise', 100, -100},
-      {'counter-clockwise', -100, 100},
+      {'clockwise', rotate_speed, rotate_speed},
+      {'counter-clockwise', -rotate_speed, -rotate_speed},
+      {'forward', forward_speed, -forward_speed},
+      {'backwards', -forward_speed, forward_speed},
       {'stop', 0, 0}
    }
+
    count = 1
 end
 
@@ -15,14 +19,14 @@ end
      It must contain the logic of your controller ]]
 function step()
    -- set velocity
-   local index = math.ceil(count / 10)
+   local index = math.ceil(count / 50)  -- 50 ticks = 10s
    local desc, left, right = table.unpack(settings[index])
    robot.differential_drive.set_target_velocity(left, right)
    print(string.format('--- step = %d, setting = %s ---', count, desc))
-   print(string.format('left:  target_vel = %d, delta_pos = %d', left, robot.differential_drive.encoders.left))
-   print(string.format('right: target_vel = %d, delta_pos = %d', right, robot.differential_drive.encoders.right))
+   print(string.format('left:  target_vel = %f, delta_pos = %f', left, robot.differential_drive.encoders.left))
+   print(string.format('right: target_vel = %f, delta_pos = %f', right, robot.differential_drive.encoders.right))
    -- increment the config variable
-   if count < 50 then
+   if count < 201 then -- 50 ticks per state * 4 states + 1 stop state
       count = count + 1
    end
 end
