e00f9aa050c8d606f2af4acf32d88e2af0d179de
sinanOguz <soguz.ankara@gmail.com>
2021-04-02 12:06:05 +0200

    removed noises

--- src/plugins/robots/drone/control_interface/ci_drone_flight_system_actuator.cpp
+++ src/plugins/robots/drone/control_interface/ci_drone_flight_system_actuator.cpp
@@ -23,16 +23,16 @@ namespace argos {
     * 1. arm (true) or disarm (false) (boolean)
     * 2. bypass safety checks (boolean)
     */
-   int LuaSetDroneFlightSystemArmed(lua_State *pt_lua_state) {
+   int LuaSetDroneFlightSystemArmed(lua_State* pt_lua_state) {
       /* Check parameters */
-      if (lua_gettop(pt_lua_state) != 2) {
+      if(lua_gettop(pt_lua_state) != 2) {
          return luaL_error(pt_lua_state, "robot.flight_system.set_armed() expects 2 arguments");
       }
       luaL_checktype(pt_lua_state, 1, LUA_TBOOLEAN);
       luaL_checktype(pt_lua_state, 2, LUA_TBOOLEAN);
       /* Get actuator instance */
-      CCI_DroneFlightSystemActuator *pcFlightSystemActuator =
-          CLuaUtility::GetDeviceInstance<CCI_DroneFlightSystemActuator>(pt_lua_state, "flight_system");
+      CCI_DroneFlightSystemActuator* pcFlightSystemActuator =
+         CLuaUtility::GetDeviceInstance<CCI_DroneFlightSystemActuator>(pt_lua_state, "flight_system");
       /* Update actuator */
       pcFlightSystemActuator->Arm(lua_toboolean(pt_lua_state, 1), lua_toboolean(pt_lua_state, 2));
       return 0;
@@ -44,18 +44,18 @@ namespace argos {
 
 #ifdef ARGOS_WITH_LUA
    /*
-    * The stack must have one value:
+    * The stack must have two values in this order:
     * 1. off-board mode enabled (true) or disabled (false) (boolean)
     */
-   int LuaSetDroneFlightSystemOffboardMode(lua_State *pt_lua_state) {
+   int LuaSetDroneFlightSystemOffboardMode(lua_State* pt_lua_state) {
       /* Check parameters */
-      if (lua_gettop(pt_lua_state) != 1) {
+      if(lua_gettop(pt_lua_state) != 1) {
          return luaL_error(pt_lua_state, "robot.flight_system.set_offboard_mode() expects 1 arguments");
       }
       luaL_checktype(pt_lua_state, 1, LUA_TBOOLEAN);
       /* Get actuator instance */
-      CCI_DroneFlightSystemActuator *pcFlightSystemActuator =
-          CLuaUtility::GetDeviceInstance<CCI_DroneFlightSystemActuator>(pt_lua_state, "flight_system");
+      CCI_DroneFlightSystemActuator* pcFlightSystemActuator =
+         CLuaUtility::GetDeviceInstance<CCI_DroneFlightSystemActuator>(pt_lua_state, "flight_system");
       /* Update actuator */
       pcFlightSystemActuator->SetOffboardMode(lua_toboolean(pt_lua_state, 1));
       return 0;
@@ -71,20 +71,20 @@ namespace argos {
     * 1. target position (vector3)
     * 2. target yaw angle (number)
     */
-   int LuaSetDroneFlightSystemTargetPose(lua_State *pt_lua_state) {
+   int LuaSetDroneFlightSystemTargetPose(lua_State* pt_lua_state) {
       /* Check parameters */
-      if (lua_gettop(pt_lua_state) != 2) {
+      if(lua_gettop(pt_lua_state) != 2) {
          return luaL_error(pt_lua_state, "robot.flight_system.set_target_pose() expects 2 arguments");
       }
       luaL_checktype(pt_lua_state, 1, LUA_TUSERDATA);
       luaL_checktype(pt_lua_state, 2, LUA_TNUMBER);
       /* Get actuator instance */
-      CCI_DroneFlightSystemActuator *pcFlightSystemActuator =
-          CLuaUtility::GetDeviceInstance<CCI_DroneFlightSystemActuator>(pt_lua_state, "flight_system");
+      CCI_DroneFlightSystemActuator* pcFlightSystemActuator =
+         CLuaUtility::GetDeviceInstance<CCI_DroneFlightSystemActuator>(pt_lua_state, "flight_system");
       /* Update actuator */
-      const CVector3 &cTargetPosition = CLuaVector3::ToVector3(pt_lua_state, 1);
+      const CVector3& cTargetPosition = CLuaVector3::ToVector3(pt_lua_state, 1);     
       pcFlightSystemActuator->SetTargetPosition(cTargetPosition);
-      pcFlightSystemActuator->SetTargetYawAngle(lua_tonumber(pt_lua_state, 2));
+      pcFlightSystemActuator->SetTargetYawAngle(lua_tonumber(pt_lua_state, 2));     
       return 0;
    }
 #endif
@@ -96,14 +96,14 @@ namespace argos {
    /*
     * The stack must have zero values
     */
-   int LuaIsDroneFlightSystemReady(lua_State *pt_lua_state) {
+   int LuaIsDroneFlightSystemReady(lua_State* pt_lua_state) {
       /* Check parameters */
-      if (lua_gettop(pt_lua_state) != 0) {
+      if(lua_gettop(pt_lua_state) != 0) {
          return luaL_error(pt_lua_state, "robot.flight_system.ready() expects 0 arguments");
       }
       /* Get actuator instance */
-      CCI_DroneFlightSystemActuator *pcFlightSystemActuator =
-          CLuaUtility::GetDeviceInstance<CCI_DroneFlightSystemActuator>(pt_lua_state, "flight_system");
+      CCI_DroneFlightSystemActuator* pcFlightSystemActuator =
+         CLuaUtility::GetDeviceInstance<CCI_DroneFlightSystemActuator>(pt_lua_state, "flight_system");
       /* Read actuator */
       lua_pushboolean(pt_lua_state, pcFlightSystemActuator->Ready());
       return 1;
@@ -114,7 +114,7 @@ namespace argos {
    /****************************************/
 
 #ifdef ARGOS_WITH_LUA
-   void CCI_DroneFlightSystemActuator::CreateLuaState(lua_State *pt_lua_state) {
+   void CCI_DroneFlightSystemActuator::CreateLuaState(lua_State* pt_lua_state) {
       CLuaUtility::OpenRobotStateTable(pt_lua_state, "flight_system");
       CLuaUtility::AddToTable(pt_lua_state, "_instance", this);
       CLuaUtility::AddToTable(pt_lua_state,
