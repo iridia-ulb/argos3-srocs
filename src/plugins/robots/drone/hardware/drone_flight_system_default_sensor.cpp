iridia-ulb/argos3-srocs

commit d6ccdf4dc3f51dd25eb9ed915c8f38d718b04bd1

Updates from Sinan
1. Update noise parameters
2. Add ENU to NED conversions for the flight system's sensor/actuator

Co-authored-by: Michael Allwright <allsey87@gmail.com>

src/plugins/robots/drone/hardware/drone_flight_system_default_sensor.cpp
@@ -89,11 +89,15 @@ namespace argos {
          const mavlink_local_position_ned_t& tReading =
             m_tLocalPositionNed.value();
          m_cPosition.Set(tReading.x, tReading.y, tReading.z);
-         m_cVelocity.Set(tReading.vx, tReading.vy, tReading.vz);
          /* set the initial position if not already set */
          if(!m_pcPixhawk->GetInitialPosition()) {
             m_pcPixhawk->GetInitialPosition().emplace(m_cPosition);
          }
+         CVector3& cInitialOrientation = m_pcPixhawk->GetInitialOrientation().value();
+         /* NED to ENU */
+         m_cPosition.RotateZ(CRadians(-cInitialOrientation.GetZ())); 
+         m_cPosition.Set(m_cPosition.GetX(), -m_cPosition.GetY(), -m_cPosition.GetZ());
+         m_cVelocity.Set(tReading.vx, -tReading.vy, -tReading.vz);
          /* clear out the read data */
          m_tLocalPositionNed.reset();
       }
@@ -101,6 +105,10 @@ namespace argos {
          const mavlink_position_target_local_ned_t &tReading =
              m_tPositionTargetLocalNed.value();
          m_cTargetPosition.Set(tReading.x, tReading.y, tReading.z);
+         CVector3& cInitialOrientation = m_pcPixhawk->GetInitialOrientation().value();
+         /* NED to ENU */
+         m_cTargetPosition.RotateZ(CRadians(-cInitialOrientation.GetZ()));
+         m_cTargetPosition.Set(m_cTargetPosition.GetX(), -m_cTargetPosition.GetY(), -m_cTargetPosition.GetZ());
          /* clear out the read data */
          m_tPositionTargetLocalNed.reset();
       }
@@ -108,13 +116,17 @@ namespace argos {
          const mavlink_attitude_t& tReading =
             m_tAttitude.value();
          m_cOrientation.Set(tReading.roll, tReading.pitch, tReading.yaw);
-         m_cAngularVelocity.Set(tReading.rollspeed,
-                                tReading.pitchspeed,
-                                tReading.yawspeed);
          /* set the initial orientation if not already set */
          if(!m_pcPixhawk->GetInitialOrientation()) {
             m_pcPixhawk->GetInitialOrientation().emplace(m_cOrientation);
          }
+         /* NED to ENU */
+         /* @Sinan TODO: double check the signs of m_cOrientation values with real tests */
+         m_cOrientation.Set(tReading.roll, -tReading.pitch, -tReading.yaw);
+         /* @Sinan TODO: double check the signs of m_cAngularVelocity values with real tests */
+         m_cAngularVelocity.Set(tReading.rollspeed,
+                                -tReading.pitchspeed,
+                                -tReading.yawspeed);
          /* clear out the read data */
          m_tAttitude.reset();
       }
@@ -124,7 +136,8 @@ namespace argos {
          CQuaternion cTargetOrientation(tReading.q[0], tReading.q[1], tReading.q[2], tReading.q[3]);
          CRadians cYaw, cPitch, cRoll;
          cTargetOrientation.ToEulerAngles(cYaw, cPitch, cRoll);
-         m_cTargetOrientation.Set(cRoll.GetValue(), cPitch.GetValue(), cYaw.GetValue());
+         /* NED to ENU */
+         m_cTargetOrientation.Set(cRoll.GetValue(), -cPitch.GetValue(), -cYaw.GetValue());
          /* clear out the read data */
          m_tAttitudeTarget.reset();
       }

src/plugins/robots/drone/hardware/drone_flight_system_default_actuator.cpp
@@ -70,7 +70,10 @@ namespace argos {
             m_pcPixhawk->GetInitialPosition().value();
          uint8_t unTargetSystem =
             m_pcPixhawk->GetTargetSystem().value();
-         CVector3& fTargetPosition = m_cTargetPosition.RotateZ(CRadians(cInitialOrientation.GetZ()));
+         CVector3& fTargetPosition = m_cTargetPosition;
+         /* ENU to NED */
+         fTargetPosition.Set(fTargetPosition.GetX(), -fTargetPosition.GetY(), -fTargetPosition.GetZ());
+         fTargetPosition.RotateZ(CRadians(cInitialOrientation.GetZ()));
          /* initialize a setpoint struct */
          mavlink_set_position_target_local_ned_t tSetpoint;
          tSetpoint.target_system    = m_pcPixhawk->GetTargetSystem().value();
@@ -80,8 +83,10 @@ namespace argos {
          tSetpoint.coordinate_frame = MAV_FRAME_LOCAL_NED;
          tSetpoint.x = fTargetPosition.GetX() + cInitialPosition.GetX();
          tSetpoint.y = fTargetPosition.GetY() + cInitialPosition.GetY();
-         tSetpoint.z = -fTargetPosition.GetZ() + cInitialPosition.GetZ();
-         tSetpoint.yaw = m_cTargetYawAngle.GetValue() + cInitialOrientation.GetZ();
+         tSetpoint.z = fTargetPosition.GetZ() + cInitialPosition.GetZ();
+         /* ENU to NED */
+         /* @Sinan TODO: double check the sign of m_cTargetYawAngle with real tests */
+         tSetpoint.yaw = -m_cTargetYawAngle.GetValue() + cInitialOrientation.GetZ();
          mavlink_message_t tMessage;
          mavlink_msg_set_position_target_local_ned_encode(unTargetSystem, 0, &tMessage, &tSetpoint);
          try {

src/plugins/robots/drone/simulator/pointmass3d_drone_model.cpp
@@ -418,23 +418,23 @@ namespace argos
    const Real CPointMass3DDroneModel::MEMS_ACCEL_NOISE_STD_DEV_Z = 0;
 #else
    /* Gyro sensor noise coefficents */
-   const Real CPointMass3DDroneModel::MEMS_GYRO_BIAS_STD_DEV = 0.15;
+   const Real CPointMass3DDroneModel::MEMS_GYRO_BIAS_STD_DEV = 0.001;
    const Real CPointMass3DDroneModel::MEMS_GYRO_BIAS_MEAN = 0;
-   const Real CPointMass3DDroneModel::MEMS_GYRO_BIAS_INIT = 0.005;
-   const Real CPointMass3DDroneModel::MEMS_GYRO_RANDOM_WALK_INIT = 0.0001;
+   const Real CPointMass3DDroneModel::MEMS_GYRO_BIAS_INIT = 0.0007;
+   const Real CPointMass3DDroneModel::MEMS_GYRO_RANDOM_WALK_INIT = 0.0007;
    const Real CPointMass3DDroneModel::MEMS_GYRO_NOISE_MEAN = 0;
-   const Real CPointMass3DDroneModel::MEMS_GYRO_NOISE_STD_DEV_X = 0.12;
-   const Real CPointMass3DDroneModel::MEMS_GYRO_NOISE_STD_DEV_Y = 0.12;
-   const Real CPointMass3DDroneModel::MEMS_GYRO_NOISE_STD_DEV_Z = 0.12;
+   const Real CPointMass3DDroneModel::MEMS_GYRO_NOISE_STD_DEV_X = 0.25;
+   const Real CPointMass3DDroneModel::MEMS_GYRO_NOISE_STD_DEV_Y = 0.25;
+   const Real CPointMass3DDroneModel::MEMS_GYRO_NOISE_STD_DEV_Z = 0.1;
    /* Accelerometer sensor noise coefficents */
-   const Real CPointMass3DDroneModel::MEMS_ACCEL_BIAS_STD_DEV = 0.15;
+   const Real CPointMass3DDroneModel::MEMS_ACCEL_BIAS_STD_DEV = 0.002;
    const Real CPointMass3DDroneModel::MEMS_ACCEL_BIAS_MEAN = 0;
-   const Real CPointMass3DDroneModel::MEMS_ACCEL_BIAS_INIT = 0.005;
-   const Real CPointMass3DDroneModel::MEMS_ACCEL_RANDOM_WALK_INIT = 0.0001;
+   const Real CPointMass3DDroneModel::MEMS_ACCEL_BIAS_INIT = 0.0009;
+   const Real CPointMass3DDroneModel::MEMS_ACCEL_RANDOM_WALK_INIT = 0.0009;
    const Real CPointMass3DDroneModel::MEMS_ACCEL_NOISE_MEAN = 0;
-   const Real CPointMass3DDroneModel::MEMS_ACCEL_NOISE_STD_DEV_X =  0.05;
-   const Real CPointMass3DDroneModel::MEMS_ACCEL_NOISE_STD_DEV_Y = 0.05;
-   const Real CPointMass3DDroneModel::MEMS_ACCEL_NOISE_STD_DEV_Z = 0.05;
+   const Real CPointMass3DDroneModel::MEMS_ACCEL_NOISE_STD_DEV_X =  0.3;
+   const Real CPointMass3DDroneModel::MEMS_ACCEL_NOISE_STD_DEV_Y = 0.3;
+   const Real CPointMass3DDroneModel::MEMS_ACCEL_NOISE_STD_DEV_Z = 0.3;
 #endif
 
    /****************************************/

commit bb48b504e34539b1ed46b513be26c689f261859d

Do not read the initial orientation before it has been initialized
Fixes a bug introduced in d6ccdf4dc3f51dd25eb9ed915c8f38d718b04bd1

Co-authored-by: Michael Allwright <allsey87@gmail.com>

src/plugins/robots/drone/hardware/drone_flight_system_default_sensor.cpp
@@ -88,27 +88,31 @@ namespace argos {
       if(m_tLocalPositionNed) {
          const mavlink_local_position_ned_t& tReading =
             m_tLocalPositionNed.value();
-         m_cPosition.Set(tReading.x, tReading.y, tReading.z);
-         /* set the initial position if not already set */
+         CVector3 cLocalPositionNed(tReading.x, tReading.y, tReading.z);
+         /* set the initial position if not already set, it should be in NED*/
          if(!m_pcPixhawk->GetInitialPosition()) {
-            m_pcPixhawk->GetInitialPosition().emplace(m_cPosition);
+            m_pcPixhawk->GetInitialPosition().emplace(cLocalPositionNed);
          }
-         CVector3& cInitialOrientation = m_pcPixhawk->GetInitialOrientation().value();
-         /* NED to ENU */
-         m_cPosition.RotateZ(CRadians(-cInitialOrientation.GetZ())); 
-         m_cPosition.Set(m_cPosition.GetX(), -m_cPosition.GetY(), -m_cPosition.GetZ());
+         if(m_pcPixhawk->GetInitialOrientation()) {
+            CVector3& cInitialOrientation = m_pcPixhawk->GetInitialOrientation().value();
+            /* NED to ENU */
+            cLocalPositionNed.RotateZ(CRadians(-cInitialOrientation.GetZ())); 
+            m_cPosition.Set(cLocalPositionNed.GetX(), -cLocalPositionNed.GetY(), -cLocalPositionNed.GetZ());
+         }
          m_cVelocity.Set(tReading.vx, -tReading.vy, -tReading.vz);
          /* clear out the read data */
          m_tLocalPositionNed.reset();
       }
       if (m_tPositionTargetLocalNed) {
-         const mavlink_position_target_local_ned_t &tReading =
-             m_tPositionTargetLocalNed.value();
-         m_cTargetPosition.Set(tReading.x, tReading.y, tReading.z);
-         CVector3& cInitialOrientation = m_pcPixhawk->GetInitialOrientation().value();
-         /* NED to ENU */
-         m_cTargetPosition.RotateZ(CRadians(-cInitialOrientation.GetZ()));
-         m_cTargetPosition.Set(m_cTargetPosition.GetX(), -m_cTargetPosition.GetY(), -m_cTargetPosition.GetZ());
+         if(m_pcPixhawk->GetInitialOrientation()) {
+            const mavlink_position_target_local_ned_t &tReading =
+                m_tPositionTargetLocalNed.value();
+            CVector3 cTargetPosition(tReading.x, tReading.y, tReading.z);
+            CVector3& cInitialOrientation = m_pcPixhawk->GetInitialOrientation().value();
+            /* NED to ENU */
+            cTargetPosition.RotateZ(CRadians(-cInitialOrientation.GetZ()));
+            m_cTargetPosition.Set(cTargetPosition.GetX(), -cTargetPosition.GetY(), -cTargetPosition.GetZ());
+         }
          /* clear out the read data */
          m_tPositionTargetLocalNed.reset();
       }